517085c807b50832f175bcb6675313fc40821bda
adriano336 <adrianochagas336@gmail.com>
2018-04-04 22:54:48 -0300

    projeto Angular 2 e 4 Loiane Groner

---
+++ hello-typescript/main.ts
@@ -0,0 +1,27 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x , y) {
+    return x + y + 1;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor +2;
+});
+
+numeros.map(valor => valor + 2);
+
+class Matematica {
+    soma(x, y) {
+        return x + y;
+    }
+}
+
+var n1: any = 'abc';
+n1 = 4;
+
+let n3: number = 400.326;
+n3 += 1;
